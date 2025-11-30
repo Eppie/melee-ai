20f1aa61472c033ad1474a7f36ff3c9437e37a22
Andrew Epstein <eppie.gov@gmail.com>
2025-11-30 14:14:01 -0500

    update zarr config

--- config/zarr_config.py
+++ config/zarr_config.py
@@ -62,7 +62,7 @@ class ZarrConfig(BaseModel):
         ),
     )
     episode_count: int = Field(
-        default=1,
+        default=6800,
         ge=1,
         description=(
             "Number of episodes (replay files) to process for training dataset. "
@@ -72,7 +72,7 @@ class ZarrConfig(BaseModel):
         ),
     )
     validation_count: int = Field(
-        default=30,
+        default=200,
         ge=1,
         description=(
             "Number of episodes to process for validation dataset. Held out from training. "
